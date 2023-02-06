5feaec9f269960e5d4e423591efdbb0329f7fdee
3than <114088911+3fallas@users.noreply.github.com>
2023-02-06 14:35:44 -0600

    Add files via upload
    Todo el codigo de la tarea.

---
+++ TareaDesarrollo-1/tentacion/src/main/java/tentacion/web/tentacion/TentacionApplication.java
@@ -0,0 +1,13 @@
+package tentacion.web.tentacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TentacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TentacionApplication.class, args);
+	}
+
+}
---
+++ TareaDesarrollo-1/tentacion/src/test/java/tentacion/web/tentacion/TentacionApplicationTests.java
@@ -0,0 +1,13 @@
+package tentacion.web.tentacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TentacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
